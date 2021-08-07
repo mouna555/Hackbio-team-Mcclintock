18c8713823752bcceda9c2cc46601e9b626c325d
Chinwendu2o <amaka013@gmail.com>
2021-08-07 13:32:46 +0100

    Change

--- Py_scripts/Ojei.py
+++ Py_scripts/Ojei.py
@@ -1,5 +1,13 @@
 
-Name = "Onyijen Ojei"
+def hammingDist(Slack_username, Twitter_handle):
+	i = 0
+	count = 0
+	while(i < len(Slack_username)):
+		if(Slack_username[i] !=Twitter_handle[i]):
+			count += 1
+		i += 1
+	return countName = "Onyijen Ojei"
+	
 Email = "ojei.onyijen@gmail.com"
 Slack_username = "@Ojei"
 Biostack = "Drug Development"
@@ -8,14 +16,7 @@ Hamming_Dist = hammingDist(Slack_username, Twitter_handle)
 print("{}, {}, {}, {}, {}, {}".format(Name, Email, Slack_username, Biostack, Twitter_handle, Hamming_Dist ))
 
 
-def hammingDist(Slack_username, Twitter_handle):
-	i = 0
-	count = 0
-	while(i < len(Slack_username)):
-		if(Slack_username[i] !=Twitter_handle[i]):
-			count += 1
-		i += 1
-	return count
+
 
 Slack_username = "@Ojei"
 Twitter_handle = "@harryjdon05"
